5a28b0ada5ca2aec5db1b37c72af3886515c87f1
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-22 23:22:17 -0300

    Sistema-Hora

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public enum tempos
+    {
+        Noturno,
+        Diurno,
+    }
+
+    public class FlagsEnumExample
+    {
+        public static void Main()
+        {
+           
+            DateTime numero = DateTime.Now;
+
+            if (numero.Hour >= 05.55 && numero.Hour < 6.10)
+            {                
+                Console.WriteLine(tempos.Noturno);
+            }
+            else if (numero.Hour >= 6.10 && numero.Hour < 16.50)
+            {
+                Console.WriteLine(tempos.Diurno);
+            }
+            else if (numero.Hour >= 16.50)
+            {
+                Console.WriteLine(tempos.Noturno);
+            }
+            else if (numero.Hour < 6.10)
+            {
+                Console.WriteLine(tempos.Noturno);
+            }
+        }
+    }
+
+}
